30b3ad9f107306d374f0cdaea9a624e2d86d87d0
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-11-05 04:37:12 -0400

    Names and Some Info Rendered to Screen - Partial Styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  font-weight: bold;
+  width: 100%;
+  color: midnightblue;
+`
+
+
+export default function Character(props){
+  const { characters } = props;
+
+  return ( characters.map(char => {
+    return <StyledDiv>
+      <h1 key={char.name}>{char.name}</h1>
+      <p>{char.birth_year}</p>
+      <p>{char.height}</p>
+      <p>{char.mass}</p>
+
+    </StyledDiv>
+
+  })
+  )
+}
